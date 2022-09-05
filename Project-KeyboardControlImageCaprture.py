a65e28adb5b5c5c311ad1eb623c79057130a1db9
pranavjain110 <jainpranav1997@gmail.com>
2022-09-04 23:08:25 -0400

    code to capture image while the drone is moving using keys, typo in KeyboardControl.py fixed

--- Project-KeyboardControlImageCaprture.py
+++ Project-KeyboardControlImageCaprture.py
@@ -1,6 +1,7 @@
 import KeyPressModule as kp
 from djitellopy import Tello
 import time
+import cv2
 
 kp.init()
 drone = Tello()
@@ -8,7 +9,10 @@ drone = Tello()
 drone.connect()
 print("Battery percentage:" ,drone.get_battery(), "\t Temperature: ",round((drone.get_temperature()-32)*5/9.0,2),"C")
 
+drone.stream_on()
 drone.takeoff()
+
+global image
 
 def getKeyboardInput():
     lr,fb,ud,yv = 0
@@ -34,10 +38,20 @@ def getKeyboardInput():
     if kp.getKey("t"): drone.takeoff()
     elif kp.getKey("l"): drone.land()
 
+    # Capturing and saving image
+    if kp.getKey("c"):
+        cv2.imwrite(f'Resources/Images/{time.time()}.jpg',img)
+        #add time delay here of 300 ms if many pics are being clicked
+
     return [lr,fb,ud,yv]
 while True:
 
-    val = getKeyboardInput()
+    img = drone.get_frame_read().frame
+    img = cv2.resize(img,(360,240))
+    #display image in a window and keep it open for 1ms
+    cv2.imshow("Drone Image",img) 
 
+    val = getKeyboardInput()
     drone.send_rc_control(val[0],val[1],val[2],val[3])  
-    time.sleep(0.1)
+
+    cv2.waitkey(1) #1ms
